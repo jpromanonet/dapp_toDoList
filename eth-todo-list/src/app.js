3f88f8375f7f185d37d6091b73b532be067585ca
jpromanonet <programming@jpromano.net>
2021-10-16 23:43:02 -0300

    Almost done

--- eth-todo-list/src/app.js
+++ eth-todo-list/src/app.js
@@ -45,6 +45,60 @@ App = {
     },
 
     loadContract: async () => {
-        const todoList 
+        const todoList = await $.getJSON('TodoList.json')
+        App.contracts.TodoList = TruffleContract(todoList)
+        App.contracts.TodoList.setProvider(App.web3Provider)
+
+        App.todoList = await App.contracts.TodoList.deployed()
+    },
+
+    render: async () => {
+        if(App.loading){
+            return
+        }
+
+        App.setLoading(true)
+
+        $('#account').html(App.account)
+
+        await App.renderTasks()
+
+        App.setLoading(false)
+    },
+
+    renderTasks: async () => {
+        // Load the total task count from the blockchain
+        const taskCount = await App.todoList.taskCount()
+        const $taskComplete = $('.taskTemplate')
+
+        for (var i = 1; i <= taskCount; i++) {
+            const task = await App.todoList.tasks(i)
+            const taskId = task[0].toNumber()
+            const taskContent = task[1]
+            const taskCompleted = task[2]
+
+            const $newTaskTemplate = $taskTemplate.clone()
+            $newTaskTemplate.find('.content').html(taskContent)
+            $newTaskTemplate.find('input')
+                            .prop('name', taskId)
+                            .prop('checked', taskCompleted)
+                            .on('click', App.toggleCompleted)
+
+            if (taskCompleted) {
+                $('#completedTaskList').append($newTaskTemplate)
+            } else {
+                $('#taskList').append($newTaskTemplate)
+            }
+
+            // Show the task
+            $newTaskTemplate.show()
+        }
+    },
+
+    createTask: async() => {
+        App.setLoading(true)
+        const content = $('#newTask').val()
+        await App.todoList.createTask(content)
+        window.location.reload()
     }
 }
