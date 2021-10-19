bf5ea94ce3ad5a6640d49b0bf40414b7c44256ec
jpromanonet <programming@jpromano.net>
2021-10-19 10:33:44 -0300

    Almost done

--- eth-todo-list/contracts/TodoList.sol
+++ eth-todo-list/contracts/TodoList.sol
@@ -1,42 +1,42 @@
-// SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity ^0.5.0;
 
 contract TodoList {
-    uint public taskCount = 0;
+  uint public taskCount = 0;
 
-    struct Task {
-        uint id;
-        string content;
-        bool completed;
-    }
+  struct Task {
+    uint id;
+    string content;
+    bool completed;
+  }
 
-    mapping(uint => Task) public tasks;
+  mapping(uint => Task) public tasks;
 
-    event TaskCreated(
-        uint id,
-        string component,
-        bool completed
-    );
+  event TaskCreated(
+    uint id,
+    string content,
+    bool completed
+  );
 
-    event TaskCompleted(
-        uint id,
-        bool completed
-    );
+  event TaskCompleted(
+    uint id,
+    bool completed
+  );
 
-    constructor() public {
-        createTask("Visitanos en nucba.com.ar =)");
-    }
+  constructor() public {
+    createTask("Check out dappuniversity.com");
+  }
 
-    function createTask(string memory _content) public {
-        taskCount ++;
-        tasks[taskCount] = Task(taskCount, _content, false);
-        emit TaskCreated(taskCount, _content, false);
-    }
+  function createTask(string memory _content) public {
+    taskCount ++;
+    tasks[taskCount] = Task(taskCount, _content, false);
+    emit TaskCreated(taskCount, _content, false);
+  }
 
-    function toggleCompleted(uint _id) public {
-        Task memory _task = tasks[_id];
-        _task.completed = !_task.completed;
-        tasks[_id] = _task;
-        emit TaskCompleted(_id, _task.completed);
-    }
+  function toggleCompleted(uint _id) public {
+    Task memory _task = tasks[_id];
+    _task.completed = !_task.completed;
+    tasks[_id] = _task;
+    emit TaskCompleted(_id, _task.completed);
+  }
+
 }
